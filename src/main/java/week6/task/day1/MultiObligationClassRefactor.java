92225cb8e81919a8f5690bd9916be0b2085d6628
masanori soeda <m.soepy@gmail.com>
2022-04-04 09:56:56 +0900

    refs #11 taskのガワ作成

---
+++ src/main/java/week6/task/day1/MultiObligationClassRefactor.java
@@ -0,0 +1,15 @@
+package week6.task.day1;
+
+/**
+ * ルール6．すべてのエンティティを小さくすること
+ *
+ * 複数責務を持つクラスは単一責務となるようにリファクタリングすべきです。
+ *
+ * ここでは有名なFizzBuzz問題を利用して体験してみます
+ *
+ * @author soepy
+ *
+ */
+public class MultiObligationClassRefactor {
+
+}
